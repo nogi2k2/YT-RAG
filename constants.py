1c5669cde9ec3df4a97910ebaf5884a0d8e1c339
nogi2k2 <ashutoshrai20bcs20@iiitkottayam.ac.in>
2025-06-19 18:05:03 +0530

    Updated constants

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-YT_EXTRACT_PAGE = "Youtube Data Extractor"
-YT_RAG_PAGE = "Youtube RAG"
+YT_EXTRACT_PAGE = "Data Scraper"
+YT_RAG_PAGE = "RAG QA"
 
 SEL_CHANL = "Select Channel"
 YT_URL_PRFX = "https://www.youtube.com/watch?v="
